91f3a0cd65348230f07ba4aa114dc0bfb6cf3175
seba <sebas.a.nunez@gmail.com>
2022-05-13 23:39:22 -0500

    add references to ubication

--- src/Layouts/Footer/index.tsx
+++ src/Layouts/Footer/index.tsx
@@ -13,27 +13,29 @@ function Footer() {
       <div className='p-10 flex flex-col gap-3'>
         <h1 className="text-white text-2xl">Ubicación</h1>
         <div className="bg-white w-16 h-1 mb-4"></div>
-        <p className='text-white'>Avenida Andrômeda, 2000. Bloco 6 e 8</p>
-        <p className='text-white'>Alphavile SP</p>
-        <p className='text-white'>brasil@corebiz.ag</p>
-        <p className='text-white'>+55 11 3090 1039</p>
-        <div className='flex flex-col w-1/2 m-auto my-10'>
-          <button type="button" className="bg-white text-black rounded-md flex p-2 mb-4">
-            <img src={mail} alt="contáctanos" className='ml-6' /> <h1 className='text-center w-full'>CONTÁCTANOS</h1>
+        <a href='https://goo.gl/maps/FnvWBiRGvMoQftP27' className='text-white text-sm w-fit'>Avenida Andrômeda, 2000. Bloco 6 e 8</a>
+        <p className='text-white text-sm w-fit'>Alphavile SP</p>
+        <a href='mailto:brasil@corebiz.ag' className='text-white text-sm w-fit'>brasil@corebiz.ag</a>
+        <a href='tel:+55 11 3090 1039' className='text-white text-sm w-fit'>+55 11 3090 1039</a>
+        <div className='flex flex-col my-10 items-center'>
+          <button type="button" className="bg-white text-black rounded-md flex p-2 mb-4 items-center h-14 w-5/6">
+            <img src={mail} alt="contáctanos" className='ml-2' />
+            <h1 className='text-center w-full text-xs font-semibold'>CONTÁCTANOS</h1>
           </button>
-          <button type="button" className="bg-white text-black rounded-md flex p-2 justify-center mb-4">
-            <img src={headset} alt="consultor" className='ml-6' /> <h1 className='text-center w-full'>HABLA CON UN CONSULTOR</h1>
+          <button type="button" className="bg-white text-black rounded-md flex p-2 mb-4 items-center h-14 w-5/6">
+            <img src={headset} alt="consultor" className='ml-2' />
+            <h1 className='text-center w-full text-xs font-semibold'>HABLA CON UN CONSULTOR</h1>
           </button>
         </div>
         <div className='flex justify-around'>
           <div>
-            <p className='text-white'>Desarrollado por</p>
+            <p className='text-white text-xs'>Desarrollado por</p>
             <img src={corebizBlack} alt="corebiz." />
           </div>
           <div>
-            <p className='text-white'>Powered by</p>
+            <p className='text-white text-xs'>Powered by</p>
             <div className='flex gap-0.5'>
-            <img src={vtex} alt="Vtex" />
+              <img src={vtex} alt="Vtex" />
               <img src={vtex_v} alt="v" />
               <img src={vtex_t} alt="t" />
               <img src={vtex_e} alt="e" />
