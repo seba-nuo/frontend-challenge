f89e9c9a9cbd41e06758e2f0b96cbfaac370b29e
seba <sebas.a.nunez@gmail.com>
2022-05-17 13:27:33 -0500

    update products to use the api

--- src/Layouts/BestSeller/index.tsx
+++ src/Layouts/BestSeller/index.tsx
@@ -6,69 +6,7 @@ import { Dots, showProducts } from "../../Services/utils";
 function BestSeller() {
   const PRODUCTS_URL = process.env.REACT_APP_PRODUCTS_URL
 
-  const [products] = useState<productsProps[]>([
-    {
-      "productId": 1,
-      "productName": "SAPATO FLOATER PRETO",
-      "stars": 1,
-      "imageUrl": "https://corebiz-test.herokuapp.com/images/product-1.png",
-      "listPrice": null,
-      "price": 25990,
-      "installments": [
-        {
-          "quantity": 9,
-          "value": 2887
-        }
-      ]
-    },
-    {
-      "productId": 2,
-      "productName": "SANDÁLIA LINHO BROWN",
-      "stars": 4,
-      "imageUrl": "https://corebiz-test.herokuapp.com/images/product-2.png",
-      "listPrice": 29900,
-      "price": 19900,
-      "installments": [
-        {
-          "quantity": 4,
-          "value": 4975
-        }
-      ]
-    },
-    {
-      "productId": 3,
-      "productName": "BOTA MUSTANG PRETO",
-      "stars": 2,
-      "imageUrl": "https://corebiz-test.herokuapp.com/images/product-3.png",
-      "listPrice": 32900,
-      "price": 29900,
-      "installments": [
-        {
-          "quantity": 10,
-          "value": 2990
-        }
-      ]
-    },
-    {
-      "productId": 4,
-      "productName": "CINTO SEMICROMO PRETO 40MM",
-      "stars": 3,
-      "imageUrl": "https://corebiz-test.herokuapp.com/images/product-4.png",
-      "listPrice": null,
-      "price": 7990,
-      "installments": []
-    },
-    {
-      "productId": 5,
-      "productName": "MEIA SPORTWEAR PRETA",
-      "stars": 5,
-      "imageUrl": "https://corebiz-test.herokuapp.com/images/product-5.png",
-      "listPrice": null,
-      "price": 1400,
-      "installments": []
-    },
-  ])
-  // const [products, setProducts] = useState<productsProps[]>()
+  const [products, setProducts] = useState<productsProps[]>([])
   const [error, setErrors] = useState(false)
   const [dotPosition, setDotPosition] = useState<number>(0)
   const [numberOfDots, setNumberOfDots] = useState(0)
@@ -85,6 +23,14 @@ function BestSeller() {
     }
   }
 
+  useEffect(() => {
+    PRODUCTS_URL && fetch(PRODUCTS_URL)
+      .then(data => data.json())
+      .then(setProducts)
+      .catch(() => setErrors(true))
+  }, [PRODUCTS_URL])
+
+  // to update items displayed in the slider based on the screen size
   useEffect(() => {
     if (mainRef.current) {
       window.addEventListener('resize', handleResize)
@@ -113,13 +59,6 @@ function BestSeller() {
 
   }, [componentWidth, products.length])
 
-
-  // useEffect(() => {
-  //   PRODUCTS_URL && fetch(PRODUCTS_URL)
-  //     .then(data => data.json())
-  //     .then(setProducts)
-  //     .catch(() => setErrors(true))
-  // }, [PRODUCTS_URL])
 
   return (
     <main className="flex flex-col" ref={mainRef}>
